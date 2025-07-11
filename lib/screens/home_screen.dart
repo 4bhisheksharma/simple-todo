1da045c752e3fd117ed53035aaa08709a2b39626
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-07-11 22:48:37 +0545

    minor adjustments

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/task_provider.dart';
+import '../widgets/task_tile.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final tasks = Provider.of<TaskProvider>(context).tasks;
 
-    final tasks = Provider.of<TasKProvider>
-
-
-
-    return Scaffold(appBar: AppBar(title: Text("TO-DO App")));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (ctx, index) {
+          final task = tasks[index];
+          return TaskTile(
+            taskTitle: task.title,
+            isDone: task.isDone,
+            onChanged: (value) {
+              // Implement task completion logic
+            }, taskId: '',
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed('/add-task');
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
